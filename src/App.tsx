5ab964bbbe6098349c8c0983956cccc71d6212ba
Manuel Montenegro <manuel@nucypher.com>
2022-11-28 13:06:36 +0100

    WIP Deploy a new strategy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import detectEthereumProvider from "@metamask/detect-provider";
+import { providers } from "ethers";
 import Posts from "./components/Posts";
+
 import {
   Cohort,
   Conditions,
@@ -15,10 +18,36 @@ function App() {
         threshold: 3,
         shares: 5,
         porterUri: "https://porter-tapir.nucypher.community",
-      }
+      };
 
-      Cohort.create(config).then((cohort) => console.log(cohort.toJSON()))
-    }
+      const nftBalanceCond = new Conditions.ERC721Balance({
+        contractAddress: "0xf5de760f2e916647fd766B4AD9E85ff943cE3A2b",
+        chain: 1,
+      });
+
+      const nftBalanceCondSet = new ConditionSet([nftBalanceCond]);
+
+      Cohort.create(config).then((cohort) => {
+        const newStrategy = Strategy.create(cohort, nftBalanceCondSet);
+
+        detectEthereumProvider().then((metamaskProvider) => {
+          const network = providers.getNetwork("maticmum");
+
+          if (metamaskProvider) {
+            const web3Provider = new providers.Web3Provider(
+              metamaskProvider,
+              network
+            );
+
+            console.time("Deploy");
+            newStrategy.deploy("test", web3Provider).then((newDeployed) => {
+              console.log(newDeployed);
+              console.timeEnd("Deploy");
+            });
+          }
+        });
+      });
+    };
 
     cohortCreate().catch(console.error);
   }, []);
