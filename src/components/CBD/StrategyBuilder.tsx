18b0bc2bad81247fa7246886c80891cccfb5501d
Manuel Montenegro <manuel@nucypher.com>
2022-12-10 21:49:30 +0100

    Make StrategyBuilder doesn't create Conditions

--- src/components/CBD/StrategyBuilder.tsx
+++ src/components/CBD/StrategyBuilder.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { Mumbai, useEthers } from "@usedapp/core";
 import detectEthereumProvider from "@metamask/detect-provider";
 import { providers } from "ethers";
 import {
   Cohort,
-  Conditions,
-  ConditionSet,
+  DeployedStrategy,
   Strategy,
 } from "@nucypher/nucypher-ts";
 
 function StrategyBuilder({ setDepStrategy, setDepStrategyStatus }: any) {
+  const { switchNetwork } = useEthers();
+
   const strategyBuild = async () => {
-    setDepStrategy("deploying...");
+    setDepStrategyStatus("deploying...");
 
     const cohortConfig = {
       threshold: 3,
@@ -18,74 +20,19 @@ function StrategyBuilder({ setDepStrategy, setDepStrategyStatus }: any) {
       porterUri: "https://porter-tapir.nucypher.community",
     };
 
+    await switchNetwork(Mumbai.chainId);
+    const web3Provider = new providers.Web3Provider(window.ethereum);
+
     const cohort = await Cohort.create(cohortConfig);
+    const strategy = Strategy.create(cohort);
 
-    // const conditionSilver = new Conditions.ERC721Balance({
-    //   contractAddress: "0xf5de760f2e916647fd766B4AD9E85ff943cE3A2b",
-    //   chain: 5,
-    //   returnValueTest: {
-    //     comparator: ">",
-    //     value: "0",
-    //   },
-    // });
-    // const conditionBronze = new Conditions.ERC721Balance({
-    //   contractAddress: "0xf5de760f2e916647fd766B4AD9E85ff943cE3A2b",
-    //   chain: 5,
-    //   returnValueTest: {
-    //     comparator: ">",
-    //     value: "1",
-    //   },
-    // });
-    // const conditionGold = new Conditions.ERC721Balance({
-    //   contractAddress: "0xf5de760f2e916647fd766B4AD9E85ff943cE3A2b",
-    //   chain: 5,
-    //   returnValueTest: {
-    //     comparator: ">",
-    //     value: "2",
-    //   },
-    // });
-    // const conditionsSilver = new ConditionSet([conditionSilver]);
-    // const conditionsBronze = new ConditionSet([conditionBronze]);
-    // const conditionsGold = new ConditionSet([conditionGold]);
+    const deployedStrategy = await strategy.deploy(
+      `blog-subscription-${Math.floor(Math.random() * 100)}`,
+      web3Provider
+    );
 
-    // const strategySilver = Strategy.create(cohort, conditionsSilver);
-    // const strategyBronze = Strategy.create(cohort, conditionsBronze);
-    // const strategyGold = Strategy.create(cohort, conditionsGold);
-
-    // const mmProvider = await detectEthereumProvider();
-    // const network = providers.getNetwork("maticmum");
-    // if (mmProvider) {
-    //   const web3Provider = new providers.Web3Provider(mmProvider, network);
-
-    //   const deployedStrategySilverProm = strategySilver.deploy(
-    //     "blog-subscription-silver",
-    //     web3Provider
-    //   );
-
-    //   const deployedStrategyBronzeProm = strategyBronze.deploy(
-    //     "blog-subscription-bronze",
-    //     web3Provider
-    //   );
-
-    //   const deployedStrategyGoldProm = strategyGold.deploy(
-    //     "blog-subscription-gold",
-    //     web3Provider
-    //   );
-
-    //   const deployedStrategySilver = await deployedStrategySilverProm;
-    //   console.log(deployedStrategySilver);
-    //   const deployedStrategyBronze = await deployedStrategyBronzeProm;
-    //   console.log(deployedStrategyBronze);
-    //   const deployedStrategyGold = await deployedStrategyGoldProm;
-    //   console.log(deployedStrategyGold);
-
-    //   setDepStrategy([
-    //     deployedStrategySilver,
-    //     deployedStrategyBronze,
-    //     deployedStrategyGold,
-    //   ]);
-    // }
-    setDepStrategyStatus("deployed");
+    setDepStrategy(deployedStrategy);
+    setDepStrategyStatus(deployedStrategy.label);
   };
 
   return (
